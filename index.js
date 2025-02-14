07391012e18a4354b2985bc6126c0c24ddc16120
vmatveev <vmatveevspam@gmail.com>
2025-02-13 19:55:51 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  
+});
+
