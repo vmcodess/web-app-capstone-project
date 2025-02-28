4ab6a68363eb0ad8d1707931234f3084134dcc32
vmatveev <vmatveevspam@gmail.com>
2025-02-28 17:03:15 -0500

    got the edit working - changed PUT to POST in my server & form for a simpler setup

--- index.js
+++ index.js
@@ -4,32 +4,74 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(express.static("/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// create object to hold posts?
+const posts = [
+  {id: 1, title: "How I Made My First Million", content: "gang gang some random shit how I made my first million. worked hard. played harder. got lucky."},
+  {id: 2, title: "Why I started breathing with my stomach.", content: "asdasd assd as das da sd as das da sd asdas da sd asd a sd a das d"}
+];
+
 
 // user will see all posts here
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  //
+  res.render("index.ejs", { posts } );
+  //res.json(posts);
 });
 
 // form to upload new post
 app.get("/new", (req, res) => {
   res.render("new.ejs");
-})
-
-// post route -- for uploading new posts
-app.post("/submit", (req, res) => {
-
 });
 
-// update route -- for editing posts
-app.put("/new", (req, res) => {
-  res.render("new.ejs");
+
+// show 1 post
+app.get("/posts/:id/edit", (req, res) => {
+  const post = posts.find(p => p.id == req.params.id);
+  res.render("edit", { post });
+});
+
+
+
+// post route -- for uploading new posts
+app.post("/posts", (req, res) => {
+  console.log(req.body);
+  let title = req.body.title;
+  let content = req.body.content;
+
+  const post = {
+    id: posts.length + 1,
+    title: title,
+    content: content
+  };
+
+  posts.push(post);
+  //console.log(`showing posts array`);
+
+  res.redirect("/");
+});
+
+
+// update route -- for editing posts (PUT)
+app.post("/posts/:id/update", (req, res) => {
+
+  const {title, content} = req.body;
+  const post = posts.find(p => p.id == req.params.id);
+  if (post) {
+    post.title = title;
+    post.content = content;
+  }
+
+  res.redirect("/");
 });
 
 // delete route -- deleting posts
-app.delete("delete", (req, res) => {
-
+app.delete("/posts/:id", (req, res) => {
+  posts = posts.filter(p => p.id != req.params.id);
+  res.redirect("/");
 });
 
 app.listen(port, () => {
